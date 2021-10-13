62f7f297604734cd285806affabf9b2e93e1c90f
Joshua Stieber <joshua.stieber.1903@gmail.com>
2021-10-13 10:30:48 +0200

    First Commits

---
+++ src/main/java/TicTacToe/TTT.java
@@ -0,0 +1,16 @@
+package TicTacToe;
+
+/**
+ *
+ * @author User#
+ */
+public class TTT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+    
+}
